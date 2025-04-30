7af34dd5ce8cac9d04d29adcc4ff5b0c5f9cff59
ringstedstamps <stampringstedogomegn@gmail.com>
2025-04-30 02:00:58 +0200

    OK

---
+++ Syddjurs/Pages/RegisterUserPage.xaml.cs
@@ -0,0 +1,137 @@
+using Syddjurs.Models;
+using System.ComponentModel;
+using System.Text;
+using System.Text.Json;
+
+namespace Syddjurs.Pages;
+
+public partial class RegisterUserPage : ContentPage, INotifyPropertyChanged
+{
+    bool _isError;
+    public bool IsError
+    {
+        get => _isError;
+        set
+        {
+            _isError = value;
+            OnPropertyChanged(nameof(IsError));
+        }
+    }
+
+    string _errorMessage;
+    public string ErrorMessage
+    {
+        get => _errorMessage;
+        set
+        {
+            _errorMessage = value;
+            OnPropertyChanged(nameof(ErrorMessage));
+        }
+    }
+
+    string _userName;
+    public string UserName
+    {
+        get => _userName;
+        set
+        {
+            _userName = value;
+            ValidateUserName();
+            OnPropertyChanged(nameof(UserName));
+        }
+    }
+
+    string _password;
+    public string Password
+    {
+        get => _password;
+        set
+        {
+            _password = value;
+            ValidatePassword();
+            OnPropertyChanged(nameof(Password));
+        }
+    }
+
+    public RegisterUserPage()
+	{
+		InitializeComponent();
+        BindingContext = this;
+    }
+
+	private async void SaveClicked(object sender, EventArgs e)
+	{		
+		var registerDto = new RegisterUserDto();
+
+        registerDto.UserName = UserName;
+        registerDto.Password = Password;
+        registerDto.Email = Email.Text;
+
+        await RegisterAsync(registerDto);
+
+    }
+
+    private  void  ValidateUserName()
+	{
+		if (UserName == "")
+		{
+            ErrorMessage = "Brugernavn skal udfyldes";
+            IsError = true; 
+            return ;	
+		}
+
+        ErrorMessage = "";
+        IsError = false;
+        return ;
+	}
+
+    private void ValidatePassword()
+    {        
+        if (Password == "")
+        {
+            ErrorMessage = "Kodeord skal udfyldes";
+            IsError = true;
+            return ;
+        }
+
+        ErrorMessage = "";
+        IsError = false;
+
+        return ;
+    }
+    public async Task RegisterAsync(RegisterUserDto dto)
+    {
+        var json = JsonSerializer.Serialize(dto);
+
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+        try
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.PostAsync("http://192.168.8.105:5000/Login/register", content);
+            if (response.IsSuccessStatusCode)
+            {
+                await Application.Current.MainPage.DisplayAlert("Success", "Kategorien er gemt", "OK");
+            }
+            else
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "Kategorien blev ikke gemt", "OK");
+            }
+        }
+
+        catch (Exception ex)
+        {
+
+        }
+    }
+
+  
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+}
