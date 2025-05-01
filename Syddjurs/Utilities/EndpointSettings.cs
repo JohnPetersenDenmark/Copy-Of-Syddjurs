5950ca510b07b815619f5cfce23d2cecd3d8bce8
JohnPetersenDenmark <johnpetersen1959@gmail.com>
2025-05-01 12:23:34 +0200

    ok

--- Syddjurs/Utilities/EndpointSettings.cs
+++ Syddjurs/Utilities/EndpointSettings.cs
@@ -8,6 +8,6 @@ namespace Syddjurs.Utilities
 {
     public static class EndpointSettings
     {
-        public static string ApiBaseUrl => "http://192.168.8.105:5000";
+        public static string ApiBaseUrl => "http://10.110.240.4:5000";
     }
 }
